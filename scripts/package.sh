3dd10d683814a38fa70c4abad5777f6e5f2f7a82
Jugheadbomb <jugheadbomb@gmail.com>
2022-12-14 14:24:08 +0800

    Fix missing config in package

--- scripts/package.sh
+++ scripts/package.sh
@@ -9,7 +9,7 @@ mkdir -p package/addons/sourcemod/translations
 
 # Copy all required stuffs to package
 cp -r addons/sourcemod/plugins/engiesvszombies.smx package/addons/sourcemod/plugins
-cp -r ../addons/sourcemod/configs/evz package/addons/sourcemod/configs
+cp -r ../addons/sourcemod/configs/evz.cfg package/addons/sourcemod/configs
 cp -r ../addons/sourcemod/gamedata/evz.txt package/addons/sourcemod/gamedata
 cp -r ../addons/sourcemod/translations/engiesvszombies.phrases.txt package/addons/sourcemod/translations
 cp -r ../sound package
